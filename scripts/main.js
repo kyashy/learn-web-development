457f9b22ea4ce9fb9d35fa51fec95c47a925545c
kyashy <kyashy4321@gmail.com>
2025-03-18 18:12:50 +0900

    Add a personalized welcome message

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,21 @@ myImage.onclick = () => {
         myImage.setAttribute("src", "images/firefox-icon.png");
     }
 }
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("ページタイトルに含めたいので、あなたの名前を教えてね！")
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `${myName}さんへ、Mozillaのご紹介`;
+}
+
+const storedName = localStorage.getItem("name");
+if (storedName) {
+    myHeading.textContent = `${storedName}さんへ、Mozillaのご紹介`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
